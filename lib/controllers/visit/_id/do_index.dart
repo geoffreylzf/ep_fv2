9f3bd7778742cff626ce75298dd265c1f944b0ad
Geoffrey <geoffreylzf@gmail.com>
2021-08-09 10:01:33 +0800

    doc observation 99%

--- lib/controllers/visit/_id/do_index.dart
+++ lib/controllers/visit/_id/do_index.dart
@@ -1,5 +1,6 @@
 import 'package:ep_fv2/db/db.dart';
 import 'package:ep_fv2/utils/xanx.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class VisitIdDoIndexController extends GetxController {
@@ -9,6 +10,9 @@ class VisitIdDoIndexController extends GetxController {
   final rxDoList = Rx<List<BroFa2Do>>([]);
   final rxVisitDoList = Rx<List<BroFa2VisitDo>>([]);
 
+  final rxDocObservationBirdQty = 0.obs;
+  final tecQty = TextEditingController();
+
   @override
   void onInit() async {
     super.onInit();
@@ -17,9 +21,96 @@ class VisitIdDoIndexController extends GetxController {
     db.broFa2VisitDoDao.watchAllByVisitId(visitId).listen((vdList) {
       rxVisitDoList.value = vdList;
     });
+
+    bool isFirst = true;
+
+    db.broFa2VisitDao.watchById(visitId).listen((v) {
+      rxDocObservationBirdQty.value = v.broFa2Visit.docObservationBirdQty ?? 0;
+      if (isFirst) {
+        if (rxDocObservationBirdQty.value == 0) {
+          openDoQtyEntryDialog();
+        }
+        isFirst = false;
+      }
+    });
+  }
+
+  @override
+  void onClose() async {
+    tecQty.dispose();
   }
 
   void openDoSubject(BroFa2Do dob) {
     Get.toNamed("${Get.currentRoute}/${dob.id}");
+  }
+
+  void openDoQtyEntryDialog() async {
+    final currentQty = rxDocObservationBirdQty.value;
+    tecQty.text = currentQty == 0 ? '' : currentQty.toString();
+    await Get.dialog(
+
+      AlertDialog(
+        title: Text('DOC Observation Qty'),
+        content: Container(
+          width: Get.width * 0.7,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              TextField(
+                controller: tecQty,
+                autofocus: true,
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "DOC Observation Bird Qty",
+                  contentPadding: EdgeInsets.all(16),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            child: Text("CANCEL"),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+          TextButton(
+            child: Text("SAVE"),
+            onPressed: () async {
+              if (tecQty.text.isEmpty) {
+                XanX.showErrorDialog(message: "Please enter qty");
+                return;
+              } else {
+                if ((int.tryParse(tecQty.text) ?? 0) == 0) {
+                  XanX.showErrorDialog(message: "Please enter valid number");
+                  return;
+                }
+              }
+
+              final visit = await db.broFa2VisitDao.getById(visitId);
+              if (visit == null) {
+                XanX.showErrorDialog(
+                    message: "Save failed, visit not exist, please restart and try");
+                return;
+              }
+
+              final qty = int.parse(tecQty.text);
+              final isUpdated =
+              await db.broFa2VisitDao.updateByPk(visit.copyWith(docObservationBirdQty: qty));
+
+              if (!isUpdated) {
+                XanX.showErrorDialog(message: "Qty is not updated, please restart and try");
+              } else {
+                Get.back();
+              }
+            },
+          )
+        ],
+      ),
+      barrierDismissible: false,
+    );
   }
 }
--- lib/pages/visits/_id/do_index.dart
+++ lib/pages/visits/_id/do_index.dart
@@ -1,4 +1,5 @@
 import 'package:ep_fv2/controllers/visit/_id/do_index.dart';
+import 'package:ep_fv2/widgets/form.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:collection/collection.dart';
@@ -12,93 +13,119 @@ class VisitIdDoIndexPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('DOC Observation'),
       ),
-      body: Obx(() {
-        final list = ctrl.rxDoList.value;
-        if (list.length == 0) {
-          return Container();
-        }
-        return ListView.separated(
-            itemCount: list.length,
-            separatorBuilder: (_, index) => Divider(height: 0),
-            itemBuilder: (ctx, position) {
-              final no = position + 1;
-              final dob = list[position];
-
-              return InkWell(
-                onTap: () {
-                  ctrl.openDoSubject(dob);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Obx(() {
-                    final widgetList = [];
-
-                    final vdList = ctrl.rxVisitDoList.value;
-                    final vd = vdList.firstWhereOrNull(
-                      (x) => x.broFa2DoId == dob.id,
-                    );
-
-                    if (vd != null) {
-                      if (vd.comment.isNotEmpty) {
-                        widgetList.add(Row(
-                          children: [
-                            Icon(Icons.comment, size: 16, color: Colors.grey)
-                                .paddingOnly(right: 4),
-                            Flexible(
-                              child: Text(
-                                vd.comment,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ));
-                      }
-
-                      if (vd.action.isNotEmpty) {
-                        widgetList.add(Row(
-                          children: [
-                            Icon(Icons.task, size: 16, color: Colors.grey).paddingOnly(right: 4),
-                            Flexible(
-                              child: Text(
-                                vd.action,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ));
-                      }
-
-                      if (vd.remark.isNotEmpty) {
-                        widgetList.add(Row(
-                          children: [
-                            Icon(Icons.feedback, size: 16, color: Colors.grey)
-                                .paddingOnly(right: 4),
-                            Flexible(
-                              child: Text(
-                                vd.remark,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
-                        ));
-                      }
-                    }
-
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          no.toString() + ". " + dob.name,
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        ...widgetList
-                      ],
-                    );
-                  }),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FormItemTitle("DOC Observation Qty"),
+                    Obx(() => Text(ctrl.rxDocObservationBirdQty.value.toString())),
+                  ],
                 ),
-              );
-            });
-      }),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  child: Text("EDIT QTY"),
+                  onPressed: () => ctrl.openDoQtyEntryDialog(),
+                ),
+              ),
+            ],
+          ).paddingAll(16),
+          Divider(height: 0),
+          Expanded(
+            child: Obx(() {
+              final list = ctrl.rxDoList.value;
+              if (list.length == 0) {
+                return Container();
+              }
+              return ListView.separated(
+                  itemCount: list.length,
+                  separatorBuilder: (_, index) => Divider(height: 0),
+                  itemBuilder: (ctx, position) {
+                    final no = position + 1;
+                    final dob = list[position];
+
+                    return InkWell(
+                      onTap: () {
+                        ctrl.openDoSubject(dob);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        child: Obx(() {
+                          final widgetList = [];
+
+                          final vdList = ctrl.rxVisitDoList.value;
+                          final vd = vdList.firstWhereOrNull(
+                            (x) => x.broFa2DoId == dob.id,
+                          );
+
+                          if (vd != null) {
+                            if (vd.comment.isNotEmpty) {
+                              widgetList.add(Row(
+                                children: [
+                                  Icon(Icons.comment, size: 16, color: Colors.grey)
+                                      .paddingOnly(right: 4),
+                                  Flexible(
+                                    child: Text(
+                                      vd.comment,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ));
+                            }
+
+                            if (vd.action.isNotEmpty) {
+                              widgetList.add(Row(
+                                children: [
+                                  Icon(Icons.task, size: 16, color: Colors.grey).paddingOnly(right: 4),
+                                  Flexible(
+                                    child: Text(
+                                      vd.action,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ));
+                            }
+
+                            if (vd.remark.isNotEmpty) {
+                              widgetList.add(Row(
+                                children: [
+                                  Icon(Icons.feedback, size: 16, color: Colors.grey)
+                                      .paddingOnly(right: 4),
+                                  Flexible(
+                                    child: Text(
+                                      vd.remark,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ));
+                            }
+                          }
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                no.toString() + ". " + dob.name,
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              ...widgetList
+                            ],
+                          );
+                        }),
+                      ),
+                    );
+                  });
+            }),
+          ),
+        ],
+      ),
     );
   }
 }
